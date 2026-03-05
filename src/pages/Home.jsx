c767b453a895a096fc590b8fd466156bd90816d3
evw91163 <180678640+evw91163@users.noreply.github.com>
2026-03-05 10:35:40 -0600

    Add src/pages/Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// Combined from adaweb4 + american-donut-association + ada
+// Home page for ADA - American Donut Association
+import { useState, useEffect } from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the American Donut Association</h1>
+    </div>
+  );
+}
